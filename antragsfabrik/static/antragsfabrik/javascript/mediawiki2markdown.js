8d7b120b42abaa982003e46fdc1ed8a68b22baaa
Christoph Giesel <mail@cgiesel.de>
2014-04-20 17:13:28 +0200

    javascript for mediawiki to markdown converting

---
+++ antragsfabrik/static/antragsfabrik/javascript/mediawiki2markdown.js
@@ -0,0 +1,22 @@
+function header_replacer(match, p1, p2, offset, string) {
+    return Array(p1.length + 1).join('#') + ' ' + p2;
+}
+
+function sublist_replacer(chars) {
+    return function (math, p1, p2, offset, string) {
+        return Array(p1.length + 1).join(' ') + chars + ' ' + p2;
+    }
+}
+
+function markdown2wiki(input) {
+    return input
+        .replace(/\*(\*+)\s*(.+?)/g, sublist_replacer('*'))
+        .replace(/#(#+)\s*(.+?)/g, sublist_replacer('1.'))
+        .replace(/#\s*(.+?)/g, '1. $1')
+        .replace(/(=+)\s*(.+?)\s*\1/g, header_replacer)
+        .replace(/'''\s*(.+?)\s*'''/g, '**$1**')
+        .replace(/''\s*(.+?)\s*''/g, '*$1*')
+        .replace(/\[\[(.+?)\|(.+?)\]\]/g, '[$2](http://wiki.piratenpartei.de/$1)')
+        .replace(/\[\[(.+?)\]\]/g, '[$1](http://wiki.piratenpartei.de/$1)')
+        .replace(/\[(.+?) (.+?)\]/g, '[$2]($1)')
+}
